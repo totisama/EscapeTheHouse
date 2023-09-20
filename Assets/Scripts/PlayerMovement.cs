a28f9418b175557f8e868bed59b341359987899d
rodrigo samayoa <toti1998sama@gmail.com>
2023-09-20 16:19:27 +0200

    Change the way to flip the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,6 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
 
-    SpriteRenderer spriteRenderer;
-
-    private void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -22,7 +15,23 @@ public class PlayerMovement : MonoBehaviour
         if (horizontal != 0.0f)
         {
             transform.position += new Vector3(horizontal * moveSpeed * Time.deltaTime, 0, 0);
-            spriteRenderer.flipX = horizontal < 0.0f;
+            FlipScale(horizontal);
         }
+    }
+
+    private void FlipScale(float horizontal)
+    {
+        Vector3 scale = transform.localScale;
+
+        if (horizontal < 0)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1;
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
     }
 }
