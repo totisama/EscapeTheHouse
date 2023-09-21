caf5dd1278ed83c05bbc5c96991305f6543c55fd
rodrigo samayoa <toti1998sama@gmail.com>
2023-09-21 19:56:26 +0200

    Add script to drag objects + changes to scene

---
+++ Assets/Scripts/Items/DragDrop.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    [SerializeField] private LayerMask draggableLayer;
+
+    private Camera cam;
+    private RaycastHit2D hit;
+    private Vector3 position;
+    private Vector3 mousePosition;
+    Transform focus;
+    bool isDrag;
+    Bounds curentRoomBounds;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        isDrag = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.GetRayIntersection(cam.ScreenPointToRay(Input.mousePosition));
+
+            if (hit.collider != null && (draggableLayer.value & (1 << hit.collider.gameObject.layer)) > 0)
+            {
+                focus = hit.transform;
+                isDrag = true;
+                curentRoomBounds = Globals.RoomBounds;
+            }
+        }
+        else if (isDrag && Input.GetMouseButtonUp(0))
+        {
+            isDrag = false;
+        }
+        else if (isDrag)
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition.z = -cam.transform.position.z;
+            position = cam.ScreenToWorldPoint(mousePosition);
+
+            // Avoid that the object can be dragged outside of the current room
+            if (position.x < curentRoomBounds.min.x)
+            {
+                position.x = curentRoomBounds.min.x;
+            }
+            else if (position.x > curentRoomBounds.max.x)
+            {
+                position.x = curentRoomBounds.max.x;
+            }
+
+            focus.position = new Vector3(position.x, focus.position.y, focus.position.z);
+        }
+    }
+}
