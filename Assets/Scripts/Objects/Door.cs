1758df8c28cb31fd33cd802b274cd5d0e253aded
rodrigo samayoa <toti1998sama@gmail.com>
2023-09-21 13:11:47 +0200

    Adding door prefab and script

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Color backgroundColor;
+    [SerializeField] private DoorSide side;
+    [Header("Sides")]
+    [SerializeField] private SpriteRenderer background;
+    [SerializeField] private SpriteRenderer front;
+    [SerializeField] private SpriteRenderer back;
+
+    enum DoorSide
+    {
+        right,
+        left
+    }
+
+    private void Awake()
+    {
+        background.color = backgroundColor;
+
+        if (side == DoorSide.left)
+        {
+            front.sortingOrder = 6;
+            back.sortingOrder = 0;
+        }
+        else
+        {
+            front.sortingOrder = 0;
+            back.sortingOrder = 6;
+        }
+    }
+}
