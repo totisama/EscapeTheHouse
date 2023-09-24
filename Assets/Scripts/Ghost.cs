84f46f9bd760a12a00552973cc099d4e84ab146f
rodrigo samayoa <toti1998sama@gmail.com>
2023-09-24 22:34:13 +0200

    add animations and sprites for ghost + WIP: ghost behavior

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private LevelRooms[] levelRooms;
+
+    //private void Update()
+    //{
+    //    Vector3 playerPosition = playerTransform.position;
+    //}
+}
