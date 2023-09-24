c7eefa0a324a49fb1c8cc2fdab8e9c2a856b52a4
rodrigo samayoa <toti1998sama@gmail.com>
2023-09-24 13:14:26 +0200

    Adding Audio manager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Sources")]
+    [SerializeField] private AudioSource backgroundSource;
+    [SerializeField] private AudioSource SFXSource;
+    [Header("Clips")]
+    [SerializeField] private SoundClip[] backgroundClips;
+    [SerializeField] private SoundClip[] SFXClips;
+    [SerializeField] private string initialBackgroundClip;
+ 
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        PlayBackground(initialBackgroundClip);
+    }
+
+    public void PlayBackground(string soundName)
+    {
+        SoundClip sound = Array.Find(backgroundClips, sound => sound.name == soundName);
+
+        if (sound == null)
+        {
+            Debug.LogFormat("No clip found for {0}", soundName);
+            return;
+        }
+
+        backgroundSource.clip = sound.clip;
+        backgroundSource.Play();
+    }
+
+    public void PlaySFXSound(string soundName)
+    {
+        SoundClip sound = Array.Find(SFXClips, x => x.name == soundName);
+
+        if (sound == null)
+        {
+            Debug.LogFormat("No clip found for {0}", soundName);
+            return;
+        }
+
+        SFXSource.PlayOneShot(sound.clip);
+    }
+}
