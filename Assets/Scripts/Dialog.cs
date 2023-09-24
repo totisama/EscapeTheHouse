074a1e1bdf10c4396fd17ee4b3c1d07d2f7df0c1
rodrigo samayoa <toti1998sama@gmail.com>
2023-09-24 20:25:27 +0200

    Adding dialog script and objects

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogPanel;
+    [SerializeField] private TMP_Text dialogText;
+    [SerializeField] private int dialogTextFont;
+    [SerializeField] private string[] dialogs;
+    [SerializeField] private string tagToSearch;
+    [Header("Typing")]
+    [SerializeField] private float typingSpeed;
+    [SerializeField] private float nextDialog;
+
+    private int currentDialog;
+    private bool opened;
+
+    private void Start()
+    {
+        dialogPanel.SetActive(false);
+        dialogText.text = string.Empty;
+    }
+
+    private void OpenDialog()
+    {
+        dialogText.fontSize = dialogTextFont;
+        opened = true;
+        dialogPanel.SetActive(true);
+        GameManager.Instance.TogglePlayer(false);
+        StartCoroutine(Typing());
+    }
+
+    private void ResetDialog()
+    {
+        dialogText.text = "";
+        currentDialog = 0;
+        dialogPanel.SetActive(false);
+        GameManager.Instance.TogglePlayer(true);
+    }
+
+    private void NextDialog()
+    {
+        if (currentDialog < dialogs.Length - 1)
+        {
+            currentDialog += 1;
+            dialogText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            ResetDialog();
+        }
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogs[currentDialog].ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        yield return new WaitForSeconds(nextDialog);
+        NextDialog();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!opened && collision.CompareTag(tagToSearch))
+        {
+            OpenDialog();
+        }
+    }
+}
