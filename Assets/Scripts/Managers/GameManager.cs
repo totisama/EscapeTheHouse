f4a19aa9e37edc56fcfb6b0d83767f5c17e0beed
rodrigo samayoa <toti1998sama@gmail.com>
2023-09-25 14:07:35 +0200

    Add ghost behavior

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -12,11 +12,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] RoomBounds startingRoom;
     [SerializeField] private TMP_Text[] mirrorTexts;
     [SerializeField] private GameObject finishUI;
+    [SerializeField] private GameObject loseUI;
+    [SerializeField] private Ghost ghost;
     [Header("Night")]
     [Tooltip("In seconds")]
     [SerializeField] int nightDuration;
 
     private int nightCooldown;
+    private int currentPlayerLevel = 1;
     private float currentNightTime;
     private float timeToCharge;
     private string safeCode;
@@ -42,6 +45,8 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         finishUI.SetActive(false);
+        loseUI.SetActive(false);
+
         startingRoom.UpdateRoomBounds();
 
         nightCooldown = machine.cooldown;
@@ -87,9 +92,10 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void ChangeRoom()
+    public void ChangeRoom(bool enterFromRight = false)
     {
         cam.UpdateCameraBounds();
+        ghost.ChangeRoom(currentPlayerLevel, enterFromRight);
     }
 
     public void ChangeNight(bool forceDay = false)
@@ -133,6 +139,7 @@ public class GameManager : MonoBehaviour
     {
         interact.canUseMachine = value;
         playerMovement.CanMove = value;
+        ghost.CanMove = value;
     }
 
     public bool CheckSafeCode(string code)
@@ -144,5 +151,42 @@ public class GameManager : MonoBehaviour
     {
         TogglePlayer(false);
         finishUI.SetActive(true);
+    }
+
+    public void UpdatePlayerLevel(int level)
+    {
+        // If the level is 2 (attic), we dont want to set it
+        if (level == 2)
+        {
+            level = 1;
+        }
+
+        currentPlayerLevel = level;
+    }
+
+    public int GetPlayerNextRoom(RoomBounds[] rooms)
+    {
+        Bounds roomBounds = Globals.RoomBounds;
+        int roomIndex = 0;
+
+        for(int i = 0; i < rooms.Length; i++)
+        {
+            Vector2 position = rooms[i].GetGhostPosition(true);
+
+            if (position.x > roomBounds.min.x && position.x < roomBounds.max.x)
+            {
+                roomIndex = i;
+                break;
+            }
+        }
+
+        return roomIndex;
+    }
+
+    public void OpenLoseCanvas()
+    {
+        TogglePlayer(false);
+        AudioManager.Instance.PlaySFXSound("Death");
+        loseUI.SetActive(true);
     }
 }
